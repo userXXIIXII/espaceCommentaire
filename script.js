47c32ea5ec780d4079ac02608f667aab07cab397
Aarin Deb <aarindeb2003@gmail.com>
2024-04-15 13:20:19 +0200

    fin du script

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 // La fonction init sera appelée lorsque l'événement DOMContentLoaded se produira
 // Récupération des éléments du fomulaire et de l'erreur
 function init() {
-    let form = document.querySelector('form');
-    let error = document.getElementById('error-message';)
+    const form = document.querySelector('form');
+    const error = document.getElementById('error-message');
 
 // Ecouter les événements sur la soumission du formulaire
 
@@ -12,11 +12,52 @@ function init() {
 
 // Récupération des éléments du formualaire espace commentaire et leurs valeurs
 
-let firstNameInput = document.getElementById('first-name').value.trim();
-let lastNameInput = document.getElementById('last-name').value.trim();
-let messageInput = document.getElementById('message').value.tim();
+const firstNameInput = document.getElementById('first-name').value.trim();
+const lastNameInput = document.getElementById('last-name').value.trim();
+const messageInput = document.getElementById('message').value.trim();
 
-if (!firstNameInput || !lastNameInput || !message) {
+// Vérifier que les champs sont remplis
+
+if (!firstNameInput || !lastNameInput || !messageInput) {
     error.style.display = 'block' ;
+    return;
 }
-    }}
+
+// Création d'un nouvel élément de commentaire
+
+const commentList = document.getElementById('comment-list');
+const newComment = document.createElement('div');
+
+// Création d'un nouvel élément du commentaire HTML
+
+newComment.innerHTML = `
+<div class="flex space-x-4 text-sm text-gray-500">
+  <div class="flex-1 py-10 border-t border-gray-200">
+    <h3 class="font-medium text-gray-900">${firstNameInput} ${lastNameInput}</h3>
+    <div class="prose prose-sm mt-4 max-w-none text-gray-500">
+      <p>${messageInput}</p>
+    </div>
+  </div>
+</div>
+`;
+
+// Ajout du nouvel élément de commentaire à la liste des commentaires
+
+commentList.appendChild(newComment);
+
+// Réinitialisation du formulaire
+
+form.reset();
+
+// Masquer le message d'erreur affiché lorsque les champs ne sont pas remplis correctement
+
+error.style.display = 'none';
+    });
+}
+
+// Garantir que le script commence à s'exécuter quand le document HTML est entièrement chargé
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+
